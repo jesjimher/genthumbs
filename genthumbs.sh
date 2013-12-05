2abee864f8ce9053e11b52f7d7a746c3aefc3118
Jesus Jimenez <jesjimenez@gmail.com>
2013-12-05 12:54:51 +0100

    Added dependence for ImageMagick Minor fixes

--- genthumbs.sh
+++ genthumbs.sh
@@ -2,15 +2,20 @@
 
 # genthumbs
 # Check for dependencies
+if ! type convert > /dev/null 2>&1; then 
+	echo "Imagemagick suite (convert) required but not installed, please install it with your package manager"
+	exit 1
+fi
+
 if ! type md5sum > /dev/null 2>&1; then 
-  echo md5sum required but not installed, please install it with your package manager
+  echo "md5sum required but not installed, please install it with your package manager"
   exit 1
 fi
 
 opti=1
 if ! type optipng > /dev/null 2>&1; then 
-	echo optipng is not installed. Execution will continue but resulting thumbnails will be larger and unoptimized
-	echo For optimal results, install optipng with your package manager
+	echo "optipng is not installed. Execution will continue but resulting thumbnails will be larger and unoptimized"
+	echo "For optimal results, install optipng with your package manager"
 	opti=0
 fi
 
